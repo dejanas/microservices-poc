9a2201f527375251a1311a0eaeff2ad6cf8d47cc
dejanastevanovic <dejana.stevanovic@flaschenpost.ch>
2024-04-20 22:33:29 +0200

    initial commit

---
+++ src/main/java/com/example/microservicespoc/MicroservicesPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicespoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/microservicespoc/MicroservicesPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicespoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
